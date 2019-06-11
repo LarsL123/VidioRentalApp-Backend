88a168b123f037c91a6bc6c2b7bc62b5c1da16b9
LarsL123 <lars@familienlien.com>
2019-06-11 20:14:46 +0200

    Integration test

--- models/rental.js
+++ models/rental.js
@@ -6,26 +6,55 @@ const Rental = mongoose.model(
   new mongoose.Schema({
     customer: {
       type: new mongoose.Schema({
-        name: { type: String, required: true, minlength: 3, maxlength: 50 },
-        isGold: { type: Boolean, required: true }
-      })
+        name: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50
+        },
+        isGold: {
+          type: Boolean,
+          default: false
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50
+        }
+      }),
+      required: true
     },
     movie: {
       type: new mongoose.Schema({
         title: {
           type: String,
           required: true,
-          minlength: 3,
-          maxlength: 50
+          trim: true,
+          minlength: 5,
+          maxlength: 255
         },
-        dailyRentalRate: { type: Number, min: 0, max: 255, required: true }
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 255
+        }
       }),
       required: true
     },
-
-    dayOut: { type: Date, default: Date.now },
-    dayIn: { type: Date },
-    rentalFee: { type: Number, min: 0 }
+    dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    dateReturned: {
+      type: Date
+    },
+    rentalFee: {
+      type: Number,
+      min: 0
+    }
   })
 );
 
